acaad12ab3795b8664d572e7a17a4b3a98f53476
Håkon Wardeberg <hakonwar@gmail.com>
2024-09-20 15:34:29 +0200

    add more cli options

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ import typer
 
 from src import show, spotter2, split3, vid, translate
 import config
+import os
 
-def main(image_path: str = "bilde.jpg"):
-    split3.split_image(image_name=image_path, visualize=True)
-    focus_points = spotter2.find_points(debug=True)
+def main(image_path: str = "bilde.jpg", visualize_split: bool = True, debug: bool = False, output_video: str ="output_video.avi"):
+    split3.split_image(image_name=image_path, visualize=visualize_split)
+    focus_points = spotter2.find_points(debug=debug)
 
     if spotter2.check_difference(focus_points):
         raise Exception("Generated focus points are too far apart")
 
     translate.translate_image_to_focus_point(focus_points)
-    vid.make_vid(name="output_video.avi")
-    show.play_video(config.OUTPUT_VIDEO_PATH + "/" + "output_video.avi")
+    vid.make_vid(name=output_video)
+    show.play_video(os.path.join(config.OUTPUT_VIDEO_PATH, "output_video.avi"))
 
 
 if __name__ == '__main__':
